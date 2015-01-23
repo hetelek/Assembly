609d365f9f7460da5b0e63b16a5b3a75bedc73fd
Adam Spindler <xmeadamx@gmail.com>
2015-01-22 20:47:23 -0500

    Create TagSearch dialog

---
+++ src/Assembly/Metro/Dialogs/TagSearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly.Metro.Dialogs
+{
+    public static class TagSearchDialog
+    {
+        public static int Show(List<string> tags)
+        {
+            var dialog = new Assembly.Metro.Dialogs.ControlDialogs.TagSearch(tags);
+            dialog.ShowDialog();
+
+            return dialog.SelectedTagIndex;
+        }
+    }
+}
